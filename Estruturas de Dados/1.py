2ab2926154ebd96cae592898554b25730677ebdb
andreluas <andreluas.k@gmail.com>
2021-03-01 21:27:23 -0300

    1 - DONE

---
+++ Estruturas de Dados/1.py
@@ -0,0 +1,22 @@
+# Avalie as seguintes expressões em Python (dica: estude a biblioteca math) 
+# a)
+a = int;
+a = 3 + 5 * 2;
+print(a);
+
+# b)
+b = int;
+b = (3 + 5) * 2;
+print(b);
+
+# c)
+import math;
+c = int;
+c = math.pow(3, 2) + 8 * 3;
+print(c);
+
+# d)
+pot = math.pow(5, 3);
+dmais = -3 - math.sqrt(pot - 2);
+dmenos = -3 + math.sqrt(pot - 2);
+print(dmais ,"and", dmenos);
